fde1c72e2f331d7eacfe72ddc8c48fc6ae20668d
Dharmendra <royd29434@gmail.com>
2026-04-30 08:42:06 +0530

    restrict CORS origin

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
